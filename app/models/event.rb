0001a18bb3cbb6f3818ebe5c6d65dff8952c91b1
Kevin Prince <nextgengames@gmail.com>
2011-07-20 18:07:35 +0100

    Fixed minor validation issue and labeling issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ActiveRecord::Base
   has_many :attendees, :dependent => :destroy
   
   validates_format_of :event_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
-  validates_format_of :venue_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates_format_of :venue_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, :allow_blank => true
   
 end
