d09e02afe0368882ba14c2b77888f4349af7d518
Kevin Prince <nextgengames@gmail.com>
2011-07-23 22:31:59 +0100

    Removing opps.html

--- app/controllers/selfservice_controller.rb
+++ app/controllers/selfservice_controller.rb
@@ -3,7 +3,7 @@ class SelfserviceController < ApplicationController
   before_filter :require_attendee
 
   def hello
-    
+    @event = @attendee.event
   end
   
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,13 @@ class Event < ActiveRecord::Base
   validates_format_of :event_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   validates_format_of :venue_url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix, :allow_blank => true
   
+  def english_date
+    
+  end
+  
+  def start_time
+    
+  end
+  
+  
 end
