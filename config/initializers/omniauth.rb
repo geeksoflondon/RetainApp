04dac2d969850fcbe2149dbe17d8d5b83673b6f4
Kevin Prince <nextgengames@gmail.com>
2011-08-28 16:59:28 +0100

    Removing App Keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'MUXGJrBvpuGOrr28ooc7gg', 'UTjlnqEScusHRfbe3o62hEBE6XJPCSc3XhghV7GcSqo'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
