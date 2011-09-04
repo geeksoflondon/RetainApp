d35f4f7855f0a1779ee1fc05509faaf7cd8dc997
Kevin Prince <nextgengames@gmail.com>
2011-09-04 17:23:27 +0100

    Added loading the heroku.yml (local env vars)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,13 @@ RetainApp::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+
+  heroku_config = File.expand_path('../../heroku.yml', __FILE__)
+  if File.exists?(heroku_config)
+    config = YAML.load_file(heroku_config)
+    config.fetch(Rails.env, {}).each do |key, value|
+      ENV[key.upcase] = value.to_s
+    end
+  end
+
 end
