00a58e08153a647a08c589933cc383adb474ca47
Kevin Prince <nextgengames@gmail.com>
2011-09-09 23:57:31 +0100

    Modifying Carrierwave for heroku

--- app/uploaders/event_logo_uploader.rb
+++ app/uploaders/event_logo_uploader.rb
@@ -10,8 +10,8 @@ class EventLogoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
   end
 
   # Create different versions of your uploaded files:
