e8a4ac1abd59773ae42bb4d9117293b0798c9779
Kevin Prince <nextgengames@gmail.com>
2011-10-29 00:36:19 +0100

    Added stats for geckoboard

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,22 @@
+class StatsController < ActionController::Base
+
+  before_filter :load_event
+
+	def onsite
+		render :layout => false, :content_type => 'application/xml'
+	end
+	
+	def load_event
+	  @event = Event.now
+    @checkins = Checkin.where('event_id = ?', params[:id])
+    @attended = @event.attended 
+		@no_shows = @event.no_show
+		@total = attended + no_shows
+		@att_conf = @event.attended_conf
+		@att_unconf = @event.attended_unconf
+		@nos_conf = @event.noshow_conf
+		@nos_unconf = @event.noshow_unconf
+		@onsite = @event.onsite
+  end
+
+end
