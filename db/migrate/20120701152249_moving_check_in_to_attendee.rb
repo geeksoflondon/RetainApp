f605338bb0a4b69af8045f5ba7b9b043c2671ebb
Kevin Prince <nextgengames@gmail.com>
2012-07-01 17:49:51 +0100

    old skool workaround

--- db/migrate/20120701152249_moving_check_in_to_attendee.rb
+++ db/migrate/20120701152249_moving_check_in_to_attendee.rb
@@ -3,25 +3,6 @@ class MovingCheckInToAttendee < ActiveRecord::Migration
     add_column :attendees, :badged, :boolean
     add_column :attendees, :onsite, :boolean
 
-    #migrate the data
-    Checkin.all.each do |checkin|
-      a = Attendee.find(checkin.attendee.id)
-      a.badged = true
-      a.onsite = false
-      a.save
-    end
-
-    #cleanup some old data issues related to checkins
-    Attendee.all.each do |attendee|
-      if attendee.status == 'attended'
-        attendee.status = 'confirmed'
-        attendee.badged = true
-        attendee.save
-      end
-    end
-
-    drop_table :checkins
-
   end
 
   def down
