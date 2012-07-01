d05ce7e2124f342f9012e61f458234d9cccc80a5
Kevin Prince <nextgengames@gmail.com>
2012-07-01 17:00:41 +0100

    First screens of admin

---
+++ app/admin/attendees.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Attendee do
+
+  filter :first_name
+  filter :last_name
+  filter :event
+  filter :status, :as => :select, :collection => proc { Attendee.status_types }
+  filter :badge, :as => :select, :collection => proc { Attendee.badge_types }
+  filter :attended
+
+  index do
+
+    column :first_name
+    column :last_name
+    column :badge
+    column :email
+    column :phone_number
+    column :status
+
+    column "Attended" do |attendee|
+     attendee.checkin.attended
+    end
+
+    column "Actions" do |attendee|
+        link_to "View", admin_attendee_path(attendee)
+        link_to "Badge"
+    end
+
+  end
+end
