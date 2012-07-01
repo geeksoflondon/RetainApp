f827025606ded1a80fcd9b354ee1680dee5241d0
Kevin Prince <nextgengames@gmail.com>
2012-07-01 11:26:56 +0100

    Removing omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  if ENV['RACK_ENV'] != 'production'
-    provider :twitter, nil, nil, :setup => true
-  else
-    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  end
-end
